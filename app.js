ca958d4e4b0a248c1b1eed70ac958b7fe132fff9
TatyCat <williams.tatyanna@gmail.com>
2018-06-20 22:50:48 -0400

    Added Delayed Results and Error Message

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-document.getElementById('calculateLoan').addEventListener('submit', calculateResults);
+document.getElementById('calculateLoan').addEventListener('submit', function(e){
+    document.getElementById('resultsSection').style.display = 'none';
+    document.getElementById('loadingGif').style.display = 'block';
 
-function calculateResults(e){
+    setTimeout(calculateResults, 1000);
+    // bc form submission, prevent default behavior
+    e.preventDefault();
+});
+
+function calculateResults(){
     const amount = document.getElementById('userLoanAmount');
     const interest = document.getElementById('userInterestAmount');
     const years = document.getElementById('userYearsRepaymentAmount');
@@ -19,28 +26,28 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal ).toFixed(2);
+        document.getElementById('loadingGif').style.display = 'none';
+        document.getElementById('resultsSection').style.display = 'block';
     }else{
+        document.getElementById('loadingGif').style.display = 'none';
         showErrorMessage('Check Your Numbers Bro');
     }
-
-    // bc form submission, prevent default behavior
-    e.preventDefault();
 }
 
-    function showErrorMessage(error){
-        const createErrorAlert = document.createElement('div');
+function showErrorMessage(error){
+    const createErrorAlert = document.createElement('div');
 
-        createErrorAlert.className = 'w3-margin-top w3-red w3-round w3-padding createdErrorBox';
-        createErrorAlert.appendChild(document.createTextNode(error));
+    createErrorAlert.className = 'w3-margin-top w3-red w3-round w3-padding w3-xlarge createdErrorBox';
+    createErrorAlert.appendChild(document.createTextNode(error));
 
-        const errorBox = document.querySelector('.errorMessageBox');
-        const errorText = document.querySelector('#errorMessageText');
+    const errorBox = document.querySelector('.errorMessageBox');
+    const errorText = document.querySelector('#errorMessageText');
 
-        errorBox.insertBefore(createErrorAlert, errorText);
+    errorBox.insertBefore(createErrorAlert, errorText);
 
-        setTimeout(clearErrorMsg, 3000);
-    }
+    setTimeout(clearErrorMsg, 2000);
+}
 
-    function clearErrorMsg(){
-        document.querySelector('.createdErrorBox').remove();
-    }
+function clearErrorMsg(){
+    document.querySelector('.createdErrorBox').remove();
+}
